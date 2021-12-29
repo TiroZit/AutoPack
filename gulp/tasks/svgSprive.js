ac9cdc1ff84002747da670af9420991e3b19a385
TiroZit <nikus.master@gmail.com>
2021-12-29 20:32:48 +0500

    init

---
+++ gulp/tasks/svgSprive.js
@@ -0,0 +1,31 @@
+import svgmin from 'gulp-svgmin';
+import svgSprite from 'gulp-svg-sprite';
+
+export const svgSprive = () => {
+  return app.gulp.src(`${app.path.src.svgicons}`, {})
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "SVG",
+        message: "Error: <%= error.message %>"
+      }))
+    )
+    .pipe(svgmin({
+			multipass: true,
+      full: true,
+      plugins: [
+        'removeDoctype',
+        'removeComments',
+        'sortAttrs',
+        'removeUselessStrokeAndFill',
+      ],
+    }))
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: `../icons/icons.svg`,
+        }
+      }
+    }))
+    .pipe(app.gulp.dest(`${app.path.build.images}`))
+    .pipe(app.plugins.browsersync.stream());
+}
