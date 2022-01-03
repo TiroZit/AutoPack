06f639f7638eb7c2d8b2f80d091991ec0a038fc2
TiroZit <nikus.master@gmail.com>
2022-01-03 15:53:01 +0500

    fixs

--- src/_js/app.js
+++ src/_js/app.js
@@ -17,9 +17,9 @@ Burger.burger();
 
 // SWIPER
 import Swiper, { Navigation, Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+//import 'swiper/css';
+//import 'swiper/css/navigation';
+//import 'swiper/css/pagination';
 
 //sliders.sliderMain();
 
