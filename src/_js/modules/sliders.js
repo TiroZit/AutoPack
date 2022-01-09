fe9e04cb1fac0c3e534a7b3c451e016693fec3eb
TiroZit <nikus.master@gmail.com>
2022-01-09 19:25:26 +0500

    clean

--- src/_js/modules/sliders.js
+++ src/_js/modules/sliders.js
@@ -1,31 +1,3 @@
-// export function sliderMain(){
-//   const sliderAbout = new Swiper('.about-slider', {
-//     grabCursor: true,
-//     loop: true,// Ьесконечный прокрут
-//     speed: 500,
-//     slidesPerView: 'auto',// Кол-во слайдов
-//     spaceBetween: 0,// Отступ между слайдами
-//     preloadImages: false,// Принудительно загрузить все слайды
-//     lazy: {
-//       loadPrevNext: true,
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     breakpoints: {
-//       // when window width is >= 640px
-//       1400: {
-//         slidesPerView: 2,
-//       }
-//     }
-//   });
-// }
-
 import Swiper, { Autoplay, Lazy, Navigation, Pagination } from 'swiper';
 
 // Инициализация слайдеров
